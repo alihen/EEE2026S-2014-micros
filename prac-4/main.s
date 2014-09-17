05714089af8175ba15e865fa8a2ffddb314271a0
Alastair Hendricks <hendricks.alastair@gmail.com>
2014-09-17 13:25:45 +0200

    push prac 4 and started prac 6

--- prac-4/main.s
+++ prac-4/main.s
@@ -15,6 +15,7 @@ vectors:
 
 _start:
         @ Initialising pins connected to LEDs outputs and pins connected to buttons to have pullups.
+
         LDR R0, RCC_Base
         LDR R1, RCC_GPB_A_En
         STR R1, [R0, #0x14]         @Enable GPIOA+B
@@ -29,41 +30,78 @@ _start:
         STR R2, [R1, #0x0]          @Set GPIOA to Input
         STR R3, [R1, #0x0C]         @Set GPIOA Pull Up Resistors
 
-
 copy_to_RAM:
         @ Copy the array specified below from flash into the start of RAM.
         @ The advised way to do it is to maintain 3 pointers: destination, source and end.
         @ Also, maintain a counter keeping track of how many bytes are moved - you'll need to know how big the array in RAM is later on.
-        LDR R0, =0x20000000
-        LDR R1, =start_of_data
-        LDR R2, =end_of_data
-loop:       
-        STR R1, [R0]
+
+        LDR R0, =0x20000000             @Set Pointer at Start of Ram
+        LDR R1, =start_of_data          @Set Source Pointer at Start of Data
+        LDR R2, =end_of_data            @Set Pointer for end of data.    
+        SUBS R2, R2, R1                   
+        ADDS R2, R2, R0                 @Calculate the last word used in ram - initially done by just comparing the end pointer, but it broke my byte add later on.
+
+copy_loop:
+        LDR R3,[R1]
+        STR R3,[R0]
+        ADDS R1, R1, #4                 @Move both pointers
         ADDS R0, R0, #4
-        ADDS R1, R1, #4
-        CMP R1, R2
-        BNE loop
+        CMP R0, R2                       @Check if we've reached the end of the loop.
+        BNE copy_loop
         
-copy_to_RAM_complete:                   @ Critical! Once the program hits this label, the automarker will modify the contents of RAM.
-
+copy_to_RAM_complete:
+        @ Critical! Once the program hits this label, the automarker will modify the contents of RAM.
         @ iterate through each individual *byte* in the array, add one to it and write it back.
-        LDR R0, =0x20000000
-        LDR R1, =start_of_data
-        LDR R2, =end_of_data
-byte_loop:       
-        LDRB R3, [R0] 
-        ADDS R3, R3, #1
-        STRB R3, [R0]
+
+        LDR R0, =0x20000000              @Reset RAM pointer to Start of RAM
+
+ byte_add:                              @Adapted from pg 114 byte copy loop in textbook
+        LDRB R1,[R0]
+        ADDS R1, R1, #1                 @increment 1 byte only.
+        STRB R1,[R0]
         ADDS R0, R0, #1
-        ADDS R1, R1, #1
-        CMP R1, R2
-        BNE byte_loop
+        CMP R0, R2                       @check if we've reached the end of the array elements in RAM.
+        BNE byte_add
 
-increment_of_bytes_complete:            @ Critical! Here, the automarker will verify that each byte has been incremented.
-
+increment_of_bytes_complete:            
+        @ Critical! Here, the automarker will verify that each byte has been incremented.
         @ The suggestion is to now iterate through the array in RAM and find the data specified in
         @ parts 3, 4 and 5. You can then store the data somewhere for use later in the program.
 
+        LDR R0, =0x20000000              @Set Pointer at Start of Ram 
+        LDRB R4, [R0]                    @Set 1st element as Max value to begin the loop
+
+get_max_unsigned_loop:
+        LDRB R1, [R0]
+        CMP R1, R4                       @Compare array element in RAM to R4 (Stored element), branch out if array element is higher
+        BHI set_max_unsigned
+        ADDS R0, R0, #1
+        CMP R0, R2
+        BNE get_max_unsigned_loop
+        
+        B reset_ram
+        
+set_max_unsigned:                       @Set new max element - Saved in R4
+        MOVS R4, R1
+        B get_max_unsigned_loop
+
+reset_ram:
+        LDR R0, =0x20000000
+        LDRB R5, [R0]                   @Set 1st element as Min value to begin the loop
+
+get_min_unsigned_loop:
+        LDRB R1, [R0]
+        CMP R1, R5
+        BCC set_min_unsigned            @If (R0 < R1) then branch BCC label / BLO label - page 109
+        ADDS R0, #1
+        CMP R0, R2
+        BNE get_min_unsigned_loop
+
+        B display_conditional_on_pushbuttons       
+           
+set_min_unsigned:
+        MOVS R5, R1
+        B get_min_unsigned_loop
 
 display_conditional_on_pushbuttons:
 
@@ -71,6 +109,9 @@ display_conditional_on_pushbuttons:
         @ The specific one which is displayed should be dependant on which button is pressed.
 
         @ infinite loop just re-checking the buttons to see if the displayed data should change. 
+
+        LDR R0, GPB_Base                @Set GPIOB to Output
+        STR R4, [R0, #0x14]             @Write to LEDs
         B display_conditional_on_pushbuttons
 
 
@@ -79,10 +120,57 @@ start_of_data:
         .word 0x22f65244
         .word 0x4e66eca3
         .word 0x25c1c308
+        .word 0xe278d1ca
+        .word 0x10e865fe
+        .word 0x839b17fb
+        .word 0xde6ac773
+        .word 0x49a0392b
+        .word 0x442b580
+        .word 0xae6e269d
+        .word 0xcb220366
+        .word 0x603debbe
+        .word 0xfd88b1bf
+        .word 0x49c5652f
+        .word 0x25476c5a
+        .word 0xa5c40771
+        .word 0xb04d908d
+        .word 0x831c1806
+        .word 0x5b4f75d4
+        .word 0x6b016b93
+        .word 0x90dcb11a
+        .word 0xefb6e394
+        .word 0x44db27da
+        .word 0xcf205f79
+        .word 0xb1192a24
+        .word 0x79cf44e2
+        .word 0x371ce3ba
+        .word 0x7a279ff5
+        .word 0x6047dc
+        .word 0xfa165142
+        .word 0x12690fdc
+        .word 0x5aad829e
+        .word 0x19244ba0
+        .word 0xb5174a3
+        .word 0xbd7172c8
+        .word 0x1d3b229f
+        .word 0xada0357e
+        .word 0x1d44e4e6
+        .word 0x37caa86e
+        .word 0x6a08fc5d
+        .word 0x465faee1
+        .word 0x2e52e372
+        .word 0xd6016409
+        .word 0x52012177
+        .word 0x848249e0
+        .word 0xcee8ec8d
+        .word 0xca09fbe7
+        .word 0x45ec4e32
+        .word 0xa11ccfb5
+        .word 0x95584228
+
 end_of_data:    @ we label the NEXT address as the end pointer. This is data we do not want to copy. 
                 @ Hence, the conditional loop should run while the source poiter is not pointing to end_of_data.
 
-RAM_Start:      .word 0x20000000    @start of ram if needed.
 GPB_Base:       .word 0x48000400    @Found at Ref Sheet Pg.41, GPIOB Resides on AHB2
 GPA_Base:       .word 0x48000000    @Found at Ref Sheet Pg.41, GPIOA Resides on AHB2
 RCC_Base:       .word 0x40021000    @Found at Ref Sheet Pg.41
---
+++ prac-6/main.s
@@ -0,0 +1,102 @@
+@STDNUM001 STDNUM002
+@ ======= MODIFY THE LINE ABOVE!!!! =======
+
+    .syntax unified
+    .global _start
+    .cpu cortex-m0
+    .thumb
+
+vectors: 
+    .word 0x20002000                @ 0x00: defines the reset value of the stack pointer
+    .word _start + 1                @ 0x04: defines the reset vector, thereby specifying the first instruction.
+    .word Default_Handler + 1       @ 0x08: NMI vector
+    .word HardFault_Handler + 1     @ 0x0C: HardFault vector
+    .word Default_Handler + 1       @ 0x10: reserved
+    .word Default_Handler + 1       @ 0x14: reserved
+    .word Default_Handler + 1       @ 0x18: reserved
+    .word Default_Handler + 1       @ 0x1C: reserved
+    .word Default_Handler + 1       @ 0x20: reserved
+    .word Default_Handler + 1       @ 0x24: reserved
+    .word Default_Handler + 1       @ 0x28: reserved
+    .word Default_Handler + 1       @ 0x2C: SVCall vector
+    .word Default_Handler + 1       @ 0x30: reserved
+    .word Default_Handler + 1       @ 0x34: reserved
+    .word Default_Handler + 1       @ 0x38: SysTick vector
+    @ you do the rest....
+
+HardFault_Handler:      @ should never happen, but handle it explcitly in case there's a bug in the code.
+        NOP
+        B HardFault_Handler
+
+@ All other exceptions should be pointed here in order to have some sort of defined behaviour in
+@ the event that they occur.
+Default_Handler:        
+        NOP
+        B Default_Handler
+
+_start:
+    @ enable clock to ADC, Timer 6, GPIOA, GPIOB
+    LDR R0, RCC_Base
+    LDR R1, RCC_GPB_A_En
+    STR R1, [R0, #0x14]         @Enable GPIOA+B
+
+    LDR R1, RCC_TIM_ADC_En
+    STR R1, [R0, #0x18]
+
+    @ set pins to correct modes
+    LDR R0, GPB_Base            @Set GPIOB to Output
+    LDR R1, GPB_Mode
+    STR R1, [R0, #0x0]
+
+    LDR R1, GPA_Base
+    LDR R2, GPA_Mode
+    STR R2, [R1, #0x0]          @Set GPIOA to Input
+    
+    @ pullups for buttons
+    LDR R3, GPA_Pull
+    STR R3, [R1, #0x0C]         @Set GPIOA Pull Up Resistors
+
+    @ enable ADC
+    
+    @ wait until ADC ready. As per Section 13.4.4: the ADC must be ready before writing to its other registers
+    @ select channel and resolution/alignment 
+    @ initialise timer: Set ARR, PSR, enable update interrupt
+    @ start counter counting
+    @ enable the interrupt for the timer in the NVIC
+
+infinite_loop:
+    NOP
+    B infinite_loop
+
+@ The following block of code is called an Interrupt Service Routine (ISR).
+@ It should be executed whenever the TIM6 IRQ occurs. 
+TIM6_DAC_IRQHandler:
+    @ acknowledge interrupt
+
+    @ ==== Part 2 suggested algoritim ====
+    @ Read GPIOB_ODR (remember, we're only interested in the LSB!)
+    @ if SW3 is held down: subtract 1 from it
+    @ else: add 1 to it.
+    @ write it back
+
+    @ ==== Part 3 suggested algorithm ====
+    @ if SW2 is held down:
+        @ kick off an ADC conversion from POT0
+        @ modify timer duration using result of conversion
+    @ else:
+        @ set the IRQ frequency to the default state
+
+    @ return from interrupt here
+
+
+.align
+
+RAM_Start:      .word 0x20000000    @start of ram if needed.
+GPB_Base:       .word 0x48000400    @Found at Ref Sheet Pg.41, GPIOB Resides on AHB2
+GPA_Base:       .word 0x48000000    @Found at Ref Sheet Pg.41, GPIOA Resides on AHB2
+RCC_Base:       .word 0x40021000    @Found at Ref Sheet Pg.41
+RCC_GPB_A_En:   .word 0x60000       @RCC_AHBENR at Ref Sheet Pg.120 [Offset 0x14] - Enable Port A and Port B
+RCC_TIM_ADC_En: .word 0x20200        @RCC_AHBENR at Ref Sheet Pg.121 [Offset 0x18] - Enable TIM and ADC
+GPB_Mode:       .word 0x5555        @GPIOx_MODER at Ref Sheet Pg.159 [Offset 0x00] [Output]
+GPA_Mode:       .word 0x28000000    @GPIOx_MODER at Ref Sheet Pg.159 [Offset 0x00] [Input]
+GPA_Pull:       .word 0x5555        @GPIOx_PUPDR at Ref Sheet Pg.161 [Offset 0x0C] - Set as Pull-Up
