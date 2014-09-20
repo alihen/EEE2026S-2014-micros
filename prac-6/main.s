859d9c261b6bd1fbda97336536e182ccde3f22a9
Alastair Hendricks <hendricks.alastair@gmail.com>
2014-09-20 03:16:59 +0200

    All but the prescaler and arr working of part 1+2

--- prac-6/main.s
+++ prac-6/main.s
@@ -41,6 +41,20 @@ vectors:
     .word Default_Handler + 1       @ 0x7C
     .word Default_Handler + 1       @ 0x80
     .word TIM6_DAC_IRQHandler + 1       @ 0x84
+    .word Default_Handler + 1       @ 0x4C
+    .word Default_Handler + 1       @ 0x50
+    .word Default_Handler + 1       @ 0x54
+    .word Default_Handler + 1       @ 0x58
+    .word Default_Handler + 1       @ 0x5C
+    .word Default_Handler + 1       @ 0x60
+    .word Default_Handler + 1       @ 0x64
+    .word Default_Handler + 1       @ 0x68
+    .word Default_Handler + 1       @ 0x6C
+    .word Default_Handler + 1       @ 0x70
+    .word Default_Handler + 1       @ 0x74
+    .word Default_Handler + 1       @ 0x78
+    .word Default_Handler + 1       @ 0x7C
+    .word Default_Handler + 1       @ 0x80
 
     @ you do the rest....
 
@@ -72,7 +86,7 @@ _start:
     STR R1, [R0, #0x0]
 
     LDR R0, GPB_Base
-    LDR R2, =#0x00              @Write to LED
+    LDR R4, =#0xFF              @Write to LED
     STR R2, [R0, #0x14]
 
     LDR R1, GPA_Base
@@ -89,8 +103,9 @@ _start:
     STR R1, [R0, #0x08]         
     
     @ wait until ADC ready. As per Section 13.4.4: the ADC must be ready before writing to its other registers
-    LDR R2, ADC_Ready
+    
 ADC_warmup:
+    LDR R2, ADC_Ready
     LDR R1, [R0, #0x00]
     ANDS R2, R2, R1
     BEQ ADC_warmup
@@ -103,22 +118,24 @@ ADC_warmup:
     STR R1, [R0, #0x0C] @set alignment and resolution
 
     @ initialise timer: Set ARR, PSR, enable update interrupt
-    LDR R0, TIM6_Base
-    LDR R1, TIM6_Prescale
+    LDR R0, TIM6_Base 
+    LDR R1, TIM6_Prescale @Where the prescale goes
     STR R1, [R0, #0x28]
 
-    LDR R1, TIM6_ARR
+    LDR R1, TIM6_ARR @Where the Arr goes
     STR R1, [R0, #0x2C]
 
-    LDR R1, TIM6_Interrupt_En
+    LDR R1, TIM6_Interrupt_En @enable the update interrupt
     STR R1, [R0, #0x0C]
 
     @ start counter counting
-    LDR R1, TIM6_Counter_En
+    LDR R1, TIM6_Counter_En @start the counter
     STR R1, [R0, #0x00]
 
     @ enable the interrupt for the timer in the NVIC
-
+    LDR R0, NVIC
+    LDR R1, NVIC_En
+    STR R1, [R0, #0x00]
 
 
     
@@ -127,33 +144,71 @@ infinite_loop:
     NOP
     B infinite_loop
 
+SW3: @subs for SW 3
+    SUBS R4, #1
+    B interrupt_cont
+
+POT_MOD:
+    @code goes here
+
+    B SW2_return
+
 @ The following block of code is called an Interrupt Service Routine (ISR).
 @ It should be executed whenever the TIM6 IRQ occurs. 
 TIM6_DAC_IRQHandler:
     @ acknowledge interrupt
+    @----THIS BLOCK OF CODE IS ONLY FOR DEBUG PORPOSES-I NEED TO GET THE PRESCALER AND ARR WORKING!
+    LDR R7,  =#0xDBD94
+timer0:
+    SUBS R7, #1
+    BNE timer0
+    @----END BLOCK - THIS BLOCK SHOULD BE DELETED ONCE THE PRESCALER IS WORKING CORRECTLY
+
+    LDR R0, TIM6_Base
+    LDR R1, Interrupt_ACK
+    STR R1, [R0, #0x10]
 
     @ ==== Part 2 suggested algoritim ====
     @ Read GPIOB_ODR (remember, we're only interested in the LSB!)
+    LDR R1, GPA_Base
+    LDRB R5, [R1, #0x10]
+    LDR R6, =#0b1000
+    ANDS R6, R6, R5
     @ if SW3 is held down: subtract 1 from it
+    BEQ SW3
     @ else: add 1 to it.
+    ADDS R4, #1
+    
+ interrupt_cont:   
     @ write it back
     LDR R0, GPB_Base
-    LDR R1, [R0, #0x14]             @Write to LED
-    LDR R2, =#0x1
-    ADDS R1, R1, R2
-    STR R1, [R0, #0x14]
+    STR R4, [R0, #0x14]
 
     @ ==== Part 3 suggested algorithm ====
     @ if SW2 is held down:
+    LDR R1, GPA_Base
+    LDRB R5, [R1, #0x10]
+    LDR R6, =#0b100
+    ANDS R6, R6, R5
+    BEQ POT_MOD
         @ kick off an ADC conversion from POT0
+        @ADC conversion code goes here
+
         @ modify timer duration using result of conversion
     @ else:
         @ set the IRQ frequency to the default state
+        LDR R0, TIM6_Base 
+        LDR R1, TIM6_Prescale @Where the prescale goes
+        STR R1, [R0, #0x28]
+
+        LDR R1, TIM6_ARR @Where the Arr goes
+        STR R1, [R0, #0x2C]
+
+ SW2_return:       
 
     @ return from interrupt here
-    LDR R0, TIM6_Base
-    LDR R1, =#0x1
-    STR R1, [R0, #0x10]
+    BX LR
+    
 
 
 .align
@@ -171,10 +226,13 @@ ADC_Ready:       .word 0x1
 ADC_CH_SEL:     .word 0x100000
 ADC_AL_RES:      .word 0x10
 TIM6_Base:       .word 0x40001000
-TIM6_Prescale:   .word 0x9C40
-TIM6_ARR:       .word 0x64
+TIM6_Prescale:   .word 0x9C3F
+TIM6_ARR:       .word 0xC8
 TIM6_Interrupt_En:  .word 0x1
 TIM6_Counter_En:    .word 0x1
+Interrupt_ACK:      .word 0x1
+NVIC:           .word 0xE000E100
+NVIC_En:        .word 0x20000
 GPB_Mode:       .word 0x5555        @GPIOx_MODER at Ref Sheet Pg.159 [Offset 0x00] [Output]
 GPA_Mode:       .word 0x28000C00   @GPIOx_MODER at Ref Sheet Pg.159 [Offset 0x00] [Input]
 GPA_Pull:       .word 0x5555        @GPIOx_PUPDR at Ref Sheet Pg.161 [Offset 0x0C] - Set as Pull-Up
